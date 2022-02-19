9dc5733c6f798c03d4c123c30eefeae17f9e79ef
Dennis Neumann <mail@dennis-neumann.com>
2022-02-19 10:57:42 +0100

    Framework for portfolio show page and update of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@
 Asset.destroy_all
 
 puts "creating assets"
-asset_1 = Asset.create!(name: "Google", category: "Share", symbol: "GOOGL")
-asset_2 = Asset.create!(name: "Apple", category: "Share", symbol: "AAPL")
-asset_2 = Asset.create!(name: "Bitcoin", category: "Crypto", symbol: "BTC")
-puts "created users"
+Asset.create!(name: "Google", category: "Share", symbol: "GOOGL")
+Asset.create!(name: "Apple", category: "Share", symbol: "AAPL")
+Asset.create!(name: "Bitcoin", category: "Crypto", symbol: "BTC")
+puts "created assets"
 
 puts "creating users"
-user_1 = User.create!(email: "rujyq@zetmail.com", password: "Frog123")
+User.create!(email: "rujyq@zetmail.com", password: "Frog123")
 puts "created users"
 
 puts "creating portfolio"
-portfolio_1 = Portfolio.create!(title: "Test", description: "Test portfolio", user_id: user_1.id)
+Portfolio.create!(title: "Test", description: "Test portfolio", user_id: 1)
 puts "created portfolio"
 
 puts "creating bookmark"
-Bookmark.create!(portfolio_id: portfolio_1.id, asset_id: asset_1.id, transaction_price: "100", transaction_type: "Buy", quantity: 1, date: 18/02/2022, comment: "test")
-Bookmark.create!(portfolio_id: portfolio_1.id, asset_id: asset_2.id, transaction_price: "150", transaction_type: "Buy", quantity: 1, date: 18/02/2022, comment: "test2")
+Bookmark.create!(portfolio_id: 1, asset_id: 1, transaction_price: 100, transaction_type: "Buy", quantity: 1, date: 18/02/2022, comment: "test")
+Bookmark.create!(portfolio_id: 1, asset_id: 2, transaction_price: 150, transaction_type: "Buy", quantity: 1, date: 18/02/2022, comment: "test2")
 puts "created bookmark"
