0870aa8afa7707965b433a1ff65d0cfc201ea65e
carlos diaz <diazmelian.carlos@gmail.com>
2022-02-19 12:08:41 +0000

    created bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    # authorize @bookmark
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # @bookmark.user = current_user
+    # authorize @bookmark
+    if @bookmark.save
+      redirect_to asset_path(@asset), notice: "Your booking has been completed 🎉"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:date, :quantity, :transaction_price, :comment, :transaction_type, :asset_id, :portfolio_id)
+  end
+
 end
