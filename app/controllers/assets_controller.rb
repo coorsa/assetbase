03a95f708eb9a6d1d401225b2ab59138ff6ee78e
Niklas Coors <coors.niklas@gmail.com>
2022-02-19 11:27:21 +0100

    pg-search added and functionality

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,2 +1,11 @@
 class AssetsController < ApplicationController
+  def index
+    @query = params[:query]
+    if @query.present?
+      @query.slice!("asset")
+      @saunas = Asset.search_by_name_category_and_symbol(@query)
+    else
+      @saunas = Asset.all
+    end
+  end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,12 @@
 class Asset < ApplicationRecord
   has_many :bookmarks
 
+  include PgSearch::Model
+  pg_search_scope :search_by_name_category_and_symbol,
+    against: [ :name, :category, :symbol ],
+    using: {
+      tsearch: { prefix: true }
+    }
   validates :name, presence: true
   validates :category, presence: true
   validates :symbol, uniqueness: true
