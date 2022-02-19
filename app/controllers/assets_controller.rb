cf557ad49186813e3fb6d44aaaebafd0dd058313
Niklas Coors <coors.niklas@gmail.com>
2022-02-19 11:43:45 +0100

    initial search functionality done with pg-search

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -3,9 +3,9 @@ class AssetsController < ApplicationController
     @query = params[:query]
     if @query.present?
       @query.slice!("asset")
-      @saunas = Asset.search_by_name_category_and_symbol(@query)
+      @assets = Asset.search_by_name_category_and_symbol(@query)
     else
-      @saunas = Asset.all
+      @assets = Asset.all
     end
   end
 
