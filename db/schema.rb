bd57ce640d5ea6ca441d7570a2f4efb35caa4d1b
Johan Lynggaard <johan.lynggaard@gmail.com>
2022-03-05 12:03:08 +0100

    Remove img_url from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_101159) do
+ActiveRecord::Schema.define(version: 2022_03_05_105124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 2022_03_05_101159) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "currency"
-    t.string "img_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
