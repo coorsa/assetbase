ccedfa8e692d67628a57ab3cd3f4b6382e3b9b86
carlos diaz <diazmelian.carlos@gmail.com>
2022-03-13 15:21:55 +0000

    fix refreshed to wrong portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,15 @@ class UsersController < ApplicationController
     if user_params[:id]
       redirect_to profile_path
     else
-      redirect_to portfolio_path
+      redirect_to portfolio_path(portfolio_param[:portfolio].to_i)
     end
   end
 
   private
+
+  def portfolio_param
+    params.require("user").permit(:portfolio)
+  end
 
   def user_params
     params.require("user").permit(:email, :name, :currency, :photo)
